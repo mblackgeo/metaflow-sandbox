05061fb996ee6cdeb77a288d755b9eaa7dbbfbdd
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-23 14:48:28 +0000

    Add minimal setup file so package is installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="footprint",
+    version="0.1.0",
+    url="https://github.com/mblackgeo/metaflow-sandbox",
+    author="Martin Black",
+    author_email="mblack@sparkgeo.com",
+    description="Create a vector footprint of a raster image",
+    packages=find_packages("footprint"),
+    install_requires=[],  # TODO
+)
