57c034a48008f9481e4413c4b6f0692bf35b9eae
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-23 15:08:43 +0000

    Fix import and type hint

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -5,15 +5,17 @@ import geopandas as gpd
 from metaflow import Flow
 from shapely.geometry import Polygon
 
+from .conftest import CliRunner
 
-def test_flow_show(runner: "CliRunner", flow_dir: str):
+
+def test_flow_show(runner: CliRunner, flow_dir: str):
     # This test does not actually execute the flow but checks that it compiles
     # correctly and returns an exit code of zero
     proc = runner.run(["flow.py", "--environment", "conda", "show"], flow_dir=flow_dir)
     assert proc.returncode == 0
 
 
-def test_flow_run(runner: "CliRunner", flow_dir: str, test_dir: str):
+def test_flow_run(runner: CliRunner, flow_dir: str, test_dir: str):
     # Run the flow with a small image
     proc = runner.run(
         ["flow.py", "--environment", "conda", "run", "--input-file", os.path.join(test_dir, "landsat.tif")],
